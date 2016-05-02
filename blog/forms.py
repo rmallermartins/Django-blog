9d5b17dc2d677833183efe040151619860102274
Rodrigo Maller <rmallermartins@gmail.com>
2016-05-02 18:00:28 -0300

    Materialized form

--- blog/forms.py
+++ blog/forms.py
@@ -8,3 +8,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'mdl-textfield__input'}),
+            'text': forms.Textarea(attrs={'class': 'mdl-textfield__input', 'rows': '5'}),
+        }
